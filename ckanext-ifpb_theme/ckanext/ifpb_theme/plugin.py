34107a4f6c0ae788d61279ed40ba274688111896
Anderson Araujo <anderson.a.g@hotmail.com>
2017-11-24 22:51:33 -0300

    Registrando Helpers

--- ckanext-ifpb_theme/ckanext/ifpb_theme/plugin.py
+++ ckanext-ifpb_theme/ckanext/ifpb_theme/plugin.py
@@ -1,14 +1,28 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+# Custom helper
+from ckanext.ifpb_theme import helpers as h
 
 class Ifpb_ThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
-
-    # IConfigurer
+    plugins.implements(plugins.ITemplateHelpers)
 
     def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'ifpb_theme')
-	toolkit.add_template_directory(config_, 'templates')
+    	toolkit.add_template_directory(config_, 'templates')
+    	toolkit.add_public_directory(config_, 'public')
+    	toolkit.add_resource('fanstatic', 'ifpb_theme')
+
+	# Registro dos helpers
+    # =======================================================
+    def get_helpers(self):
+        '''Register all functions
+        '''
+        
+        # Template helper function names should begin with the name of the
+        # extension they belong to, to avoid clashing with functions from
+        # other extensions.
+        return {
+            # Homepage
+            'count_organizations': h.organization.count_organizations
+        }
