2681b95dd5adf8365d61a12b27b94ea83c8fa815
Anderson Araujo <anderson.a.g@hotmail.com>
2017-12-22 00:01:15 -0300

    Add rotas, autorizações e actions

--- ckanext-ifpb_theme/ckanext/ifpb_theme/plugin.py
+++ ckanext-ifpb_theme/ckanext/ifpb_theme/plugin.py
@@ -1,14 +1,22 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+from routes.mapper import SubMapper
+
 # Custom helper
 from ckanext.ifpb_theme import helpers as h
+
+from ckanext.ifpb_theme.logic.auth import create as auth_create
+from ckanext.ifpb_theme.logic.action import create as action_create
+from ckanext.ifpb_theme.logic.action import get as action_get
 
 
 class Ifpb_ThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.ITemplateHelpers)
     plugins.implements(plugins.IRoutes)
+    plugins.implements(plugins.IAuthFunctions)
+    plugins.implements(plugins.IActions)
 
     def update_config(self, config_):
     	toolkit.add_template_directory(config_, 'templates')
@@ -36,8 +44,23 @@ class Ifpb_ThemePlugin(plugins.SingletonPlugin):
 
     def after_map(self, map):
         # App
-        map.connect('app', '/aplicativos',
-                    controller='ckanext.ifpb_theme.controllers.aplicativos:AplicativosController',
-                    action='index')
+        map.redirect('/app', '/apps')
+        with SubMapper(map, controller='ckanext.ifpb_theme.controllers.app:AppController') as m:
+            m.connect('app_show', '/apps', action='show')
+            m.connect('app_new', '/app/new', action='new')
+
                     
         return map
+
+    # Definir autorization
+    # =======================================================                
+    def get_auth_functions(self):
+        return {
+            'app_create': auth_create.app_create
+            }
+
+    # Definir novas actions
+    # ========================================================
+    def get_actions(self):
+        return {'app_create': action_create.app_create,
+                'app_list': action_get.app_list}
