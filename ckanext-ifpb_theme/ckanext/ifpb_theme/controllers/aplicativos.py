a83989441b89ff35b3c1a2b024ca6fc1e7266355
Anderson Araujo <anderson.a.g@hotmail.com>
2017-12-21 23:59:20 -0300

    deletado

--- ckanext-ifpb_theme/ckanext/ifpb_theme/controllers/aplicativos.py
+++
@@ -1,8 +0,0 @@
-import ckan.plugins as p
-
-# ============================================================
-# Aplicativos
-# ============================================================
-class AplicativosController(p.toolkit.BaseController):
-    def index (self):
-		return p.toolkit.render('aplicativo/index.html')
