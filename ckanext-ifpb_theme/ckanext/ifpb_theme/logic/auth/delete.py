044704a994a443698f1a2e9b58b70378ea8e3b5c
Anderson Araujo <anderson.a.g@hotmail.com>
2017-12-26 23:46:57 -0300

    Add new auth

---
+++ ckanext-ifpb_theme/ckanext/ifpb_theme/logic/auth/delete.py
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+import ckan.logic as logic
+import ckan.authz as authz
+import ckan.logic.auth as logic_auth
+
+from ckan.common import _
+
+def app_delete(context, data_dict=None):
+    user = context['user']
+    user = authz.get_user_id_for_username(user, allow_none=True)
+
+    if user and authz.check_config_permission('user_delete_apps'):
+        return {'success': True}
+    return {'success': False,
+            'msg': _('Usuário %s não autorizado para excluir o app') % user}
